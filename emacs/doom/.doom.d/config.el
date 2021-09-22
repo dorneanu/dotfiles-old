31de944f662946203e578eb54b25136c17e917e0
Victor Dorneanu <info@dornea.nu>
2021-09-22 09:26:07 +0200

    Setting default font for X

--- emacs/doom/.doom.d/config.el
+++ emacs/doom/.doom.d/config.el
@@ -39,6 +39,12 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "Source Code Pro for Powerline" :size 12 :weight 'normal)
 ;;       doom-variable-pitch-font (font-spec :family "Source Code Pro for Powerline" :size 13))
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;         doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13))
+
+;; Set fonts
+;; (set-face-font 'default "Source Code Pro Medium 15")
+(setq doom-font (font-spec :family "Source Code Pro" :size 17))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
