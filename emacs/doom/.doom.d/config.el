801618c41108c947de61db51d7c86db6c97c5bd0
Victor Dorneanu <info@dornea.nu>
2021-10-05 08:14:26 +0200

    Setup clipboard settings

--- emacs/doom/.doom.d/config.el
+++ emacs/doom/.doom.d/config.el
@@ -51,6 +51,11 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 
+
+;; set clipboard settings
+(require 'xclip)
+(xclip-mode 1)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/work/repos/org/")
